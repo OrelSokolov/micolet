964939e425320c55dad3629f49e9d65e02ec35ec
Administrator <orelcokolov@gmail.com>
2021-05-14 15:14:21 +0300

    Require preferences

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,10 +1,17 @@
 class Lead < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validate :email_should_be_email
+  validate :prefs_required
 
   def email_should_be_email
     unless URI::MailTo::EMAIL_REGEXP.match?(email)
       errors.add(:email, I18n.t('lead.errors.email'))
+    end
+  end
+
+  def prefs_required
+    unless womans_fashion.present? || mens_fashion.present? || children_fashion.present?
+      errors.add(:preferences, I18n.t('lead.errors.preferences_required'))
     end
   end
 end
